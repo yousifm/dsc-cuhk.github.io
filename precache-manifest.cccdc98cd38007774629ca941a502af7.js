2fb9f860c12b241976431da40904ba583fd88472
Youssef Mahmoud <ymahmoud1b@gmail.com>
2019-10-08 22:48:32 +0800

    Updates

---
+++ precache-manifest.cccdc98cd38007774629ca941a502af7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b0772e77e3b3aa467394d5e02a0901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b68472a63e1537205b2",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "8b68472a63e1537205b2",
+    "url": "/static/js/2.776b79d4.chunk.js"
+  },
+  {
+    "revision": "0025184a7e29c44c3549",
+    "url": "/static/js/main.212ffcb2.chunk.js"
+  },
+  {
+    "revision": "cb6f882110a60c82fff2",
+    "url": "/static/js/runtime-main.2587b28f.js"
+  }
+]);
